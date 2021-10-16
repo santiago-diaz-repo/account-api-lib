e0f07226c5dd6dc54d91bf6c28aa28771d530cf3
santiago-diaz-repo <santiago.diaz5689@gmail.com>
2021-10-16 12:37:55 -0500

    :sparkles: Creation of builder configuration

---
+++ configuration/config_builder.go
@@ -0,0 +1,64 @@
+package configuration
+
+import (
+	"net/http"
+	"time"
+)
+
+type ConfigBuilder interface {
+	WithHttpClient(*http.Client) ConfigBuilder
+	WithAPIVersion(string) ConfigBuilder
+	WithPort(string) ConfigBuilder
+	Verbose() ConfigBuilder
+	Build(string) Config
+}
+
+type configBuilderStruct struct {
+	config
+}
+
+const (
+	DefaultAPIVersion = "v1"
+	DefaultPort       = "80"
+	DefaultTimeout    = 4 * time.Second
+	DefaultVerbose    = false
+)
+
+func NewConfigBuilder() ConfigBuilder {
+	configBuilder := new(configBuilderStruct)
+	configBuilder.config.port = DefaultPort
+	configBuilder.config.apiVersion = DefaultAPIVersion
+	configBuilder.config.verboseLog = DefaultVerbose
+	return configBuilder
+}
+
+func (c *configBuilderStruct) WithHttpClient(httpClient *http.Client) ConfigBuilder {
+	c.config.httpClient = httpClient
+	return c
+}
+
+func (c *configBuilderStruct) WithAPIVersion(apiVersion string) ConfigBuilder {
+	c.config.apiVersion = apiVersion
+	return c
+}
+
+func (c *configBuilderStruct) WithPort(port string) ConfigBuilder {
+	c.config.port = port
+	return c
+}
+
+func (c *configBuilderStruct) Verbose() ConfigBuilder {
+	c.config.verboseLog = true
+	return c
+}
+
+func (c *configBuilderStruct) Build(host string) Config {
+	if c.config.httpClient == nil {
+		c.config.httpClient = NewDefaultHttpClient(DefaultTimeout, c.verboseLog)
+	}
+	c.config.httpClient.Transport
+
+	c.config.host = host
+
+	return &c.config
+}
---
+++ configuration/config_builder_test.go
@@ -0,0 +1,131 @@
+package configuration
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestConfigBuilder_ShouldAssignNewHttpClient(t *testing.T) {
+	subject := configBuilderStruct{
+		config{
+			httpClient: &http.Client{},
+		},
+	}
+
+	want := &http.Client{}
+	subject.WithHttpClient(want)
+	got := subject.config.httpClient
+
+	if got != want {
+		t.Errorf("wanted: %p\n got: %p", want, got)
+	}
+}
+
+func TestConfigBuilder_ShouldAssignNewAPIVersion(t *testing.T) {
+	subject := configBuilderStruct{
+		config{
+			apiVersion: "v1",
+		},
+	}
+
+	want := "v2"
+	subject.WithAPIVersion(want)
+	got := subject.config.apiVersion
+
+	if got != want {
+		t.Errorf("wanted: %s\n got: %s", want, got)
+	}
+}
+
+func TestConfigBuilder_ShouldAssignNewPort(t *testing.T) {
+	subject := configBuilderStruct{
+		config{
+			port: "80",
+		},
+	}
+
+	want := "8080"
+	subject.WithPort(want)
+	got := subject.config.port
+
+	if got != want {
+		t.Errorf("wanted: %s\n got: %s", want, got)
+	}
+}
+
+func TestConfigBuilder_ShouldEnableVerbose(t *testing.T) {
+	subject := configBuilderStruct{
+		config{
+			verboseLog: false,
+		},
+	}
+
+	want := true
+	subject.Verbose()
+	got := subject.config.verboseLog
+
+	if got != want {
+		t.Errorf("wanted: %t\n got: %t", want, got)
+	}
+}
+
+func TestConfigBuilder_ShouldReturnBasicConfigImplementation(t *testing.T) {
+	want := &config{
+		host:       "localhost",
+		apiVersion: "v1",
+		port:       "80",
+		verboseLog: false,
+		httpClient: &http.Client{Timeout: 4 * time.Second},
+	}
+
+	subject := NewConfigBuilder().
+		Build("localhost")
+	valueType := reflect.ValueOf(subject)
+	got := valueType.Interface().(*config)
+
+	if got.host != want.host {
+		t.Errorf("host wanted: %s\n host got: %s", want.host, got.host)
+	}
+
+	if got.apiVersion != want.apiVersion {
+		t.Errorf("apiVersion wanted: %s\n apiVersion got: %s", want.apiVersion, got.apiVersion)
+	}
+
+	if got.verboseLog != want.verboseLog {
+		t.Errorf("verboseLog wanted: %t\n verboseLog got: %t", want.verboseLog, got.verboseLog)
+	}
+
+	if got.port != want.port {
+		t.Errorf("port wanted: %s\n port got: %s", want.port, got.port)
+	}
+
+	if got.httpClient.Timeout != want.httpClient.Timeout {
+		t.Errorf("client timeout wanted: %s\n client timeout got: %s", want.httpClient.Timeout, got.httpClient.Timeout)
+	}
+}
+
+func TestConfigBuilder_ShouldReturnCustomConfigImplementation(t *testing.T) {
+	want := &config{
+		host:       "test",
+		apiVersion: "v2",
+		port:       "8080",
+		verboseLog: true,
+		httpClient: &http.Client{},
+	}
+
+	subject := NewConfigBuilder().
+		WithHttpClient(want.httpClient).
+		WithPort("8080").
+		WithAPIVersion("v2").
+		Verbose().
+		Build("test")
+
+	valueType := reflect.ValueOf(subject)
+	got := valueType.Interface().(*config)
+
+	if !reflect.DeepEqual(*got, *want) {
+		t.Errorf("wanted: %#v\n got: %#v", want, got)
+	}
+}
