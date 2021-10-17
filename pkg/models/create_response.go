f7ebcf76c028a39b152f0342aa08a61c780b7627
santiago-diaz-repo <santiago.diaz5689@gmail.com>
2021-10-17 12:16:05 -0500

    :construction: remove error message from response models

--- pkg/models/create_response.go
+++ pkg/models/create_response.go
@@ -1,6 +1,6 @@
 package models
 
 type CreateResponse struct {
-	ResBody      *ResponseObject
-	StatusCode   int
+	ResBody    *ResponseObject
+	StatusCode int
 }
--- pkg/models/delete_response.go
+++ pkg/models/delete_response.go
@@ -1,5 +1,5 @@
 package models
 
 type DeleteResponse struct {
-	StatusCode   int
+	StatusCode int
 }
