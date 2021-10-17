a7bcd01e0ad5b9ad5bc3ceda2ca27e392b0442a0
santiago-diaz-repo <santiago.diaz5689@gmail.com>
2021-10-17 10:21:41 -0500

    :contruction: Create custom error struct that implements error interface

---
+++ pkg/error_handling/account_error.go
@@ -0,0 +1,33 @@
+package error_handling
+
+import "fmt"
+
+type AccountError struct {
+	operation string
+	code int
+	message string
+}
+
+func NewAccountError(operation string, code int, message string) error{
+	return &AccountError{
+		operation: operation,
+		code: code,
+		message: message,
+	}
+}
+
+func (ce *AccountError) Error() string {
+	return fmt.Sprintf("%s : %d - %s",ce.operation, ce.code,ce.message)
+}
+
+func (ce *AccountError) GetOperation() string{
+	return ce.operation
+}
+
+func (ce *AccountError) GetCode() int {
+	return ce.code
+}
+
+func (ce *AccountError) GetMessage() string{
+	return ce.message
+}
---
+++ pkg/error_handling/account_error_test.go
@@ -0,0 +1,72 @@
+package error_handling
+
+import (
+	"reflect"
+	"testing"
+)
+
+func getAccountError() AccountError{
+	return AccountError{
+		operation: "test",
+		code: 1,
+		message: "test error",
+	}
+}
+
+func TestAccountError_ShouldReturnANewAccountError(t *testing.T) {
+
+	want := "*error_handling.AccountError"
+	subject := NewAccountError("test",1,"test")
+	got := reflect.TypeOf(subject).String()
+
+	if  got != want {
+		t.Errorf("wanted: %v\n got: %v", want, got)
+	}
+}
+
+func TestAccountError_ShouldReturnErrorMessage(t *testing.T) {
+
+	want := "test : 1 - test error"
+	subject := getAccountError()
+	 got := subject.Error()
+
+	if  got != want {
+		t.Errorf("wanted: %v\n got: %v", want, got)
+	}
+}
+
+func TestAccountError_ShouldReturnCode(t *testing.T) {
+
+	want := 1
+	subject := getAccountError()
+
+	got := subject.GetCode()
+
+	if  got != want {
+		t.Errorf("wanted: %v\n got: %v", want, got)
+	}
+}
+
+func TestAccountError_ShouldReturnMessage(t *testing.T) {
+
+	want := "test error"
+	subject := getAccountError()
+
+	got := subject.GetMessage()
+
+	if  got != want {
+		t.Errorf("wanted: %v\n got: %v", want, got)
+	}
+}
+
+func TestAccountError_ShouldReturnOperation(t *testing.T) {
+
+	want := "test"
+	subject := getAccountError()
+
+	got := subject.GetOperation()
+
+	if  got != want {
+		t.Errorf("wanted: %v\n got: %v", want, got)
+	}
+}
